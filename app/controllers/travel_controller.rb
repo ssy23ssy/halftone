9d8fe299a1413428f6ffeed2afef393f42c57f92
신승륜 <ssr@sinseunglyun-ui-MacBook-Pro.local>
2017-05-21 14:45:41 +0900

    ssss

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -5,13 +5,39 @@ class TravelController < ApplicationController
   end
 
   def new
-    @travel = Travel.new
+  end
+
+  def create
+    @user = User.find(current_user.id)
+    @travel = @user.travels.new(title: params[:travel][:title],
+                         country: params[:travel][:country],
+                         period: params[:travel][:period].to_i,
+                         food: params[:travel][:food].to_i,
+                         sleep: params[:travel][:sleep].to_i,
+                         trans: params[:travel][:trans].to_i,
+                         free: params[:travel][:free].to_i)
+    @travel.total = @travel.period + @travel.food + @travel.sleep + @travel.trans + @travel.free
+    @travel.save
+
+    redirect_to new_travel_path
+  end
+
+  def mylist
+    @user = User.find(current_user.id)
+    @travels = @user.travels
+  end
+
+  def show
   end
 
   private
 
-  def travel_params
-    params.require(:travel).permit(:title)
+  def travel_params_s
+    params.require(:travel).permit(:title, :country)
+  end
+
+  def travel_params_i
+    params.require(:travel).permit(:period, :food, :sleep, :trans, :free)
   end
  
 end
