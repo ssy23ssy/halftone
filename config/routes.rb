9f1df933371803a4a87e6ad6ec97dd5a1c9761bb
신승륜 <ssr@sinseunglyun-ui-MacBook-Pro.local>
2017-05-21 16:17:45 +0900

    ssssss

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'travel#index'
   get 'travel/mylist'
-  get 'travel/new'
-  
+
   resources :travel, except: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
